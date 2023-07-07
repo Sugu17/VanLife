f0ea00af1c1c4c6d1050cff83f6e18170017e356
Wade Wilson <sugumarsg17@gmail.com>
2023-07-08 03:57:49 +0530

    Fix - Host Layout padding

---
+++ src/Pages/host/vans/HostVansPage.tsx
@@ -0,0 +1,10 @@
+function HostVansPage() {
+  return (
+    <div>
+      <h2 className="text-[32px] text-neutral-950 font-bold">
+        Your listed vans
+      </h2>
+    </div>
+  );
+}
+export default HostVansPage;
