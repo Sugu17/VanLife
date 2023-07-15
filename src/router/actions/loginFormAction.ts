2a9cf3c9cd058623bcf450bdbd190e43f93adf4e
Wade Wilson <sugumarsg17@gmail.com>
2023-07-15 13:57:36 +0530

    Fix-Import for production

--- src/router/actions/loginFormAction.ts
+++ src/router/actions/loginFormAction.ts
@@ -1,12 +1,12 @@
 import { redirect } from "react-router-dom";
 
-interface FormActionArgs {
-  params: object;
-  request: Request;
-}
-
-export default async function loginFormAction({ request }: FormActionArgs) {
-  const formData = await request.formData();
+// interface FormActionArgs {
+//   params: object;
+//   request: Request;
+// }
+// export default async function loginFormAction({ request }: FormActionArgs) {
+export default async function loginFormAction() {
+  // const formData = await request.formData();
   // const name = formData.get("email");
   // const password = formData.get("password");
   localStorage.setItem("user", "Wade");
