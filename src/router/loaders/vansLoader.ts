a621773245579d59c83079d5b361738e81bc9432
Wade Wilson <sugumarsg17@gmail.com>
2023-07-16 06:14:43 +0530

    Rf-Pass actual vansData instead of pre-fetching

--- src/router/loaders/vansLoader.ts
+++ src/router/loaders/vansLoader.ts
@@ -1,11 +1,32 @@
 import { QueryClient } from "@tanstack/react-query";
+import { defer } from "react-router-dom";
+import { data } from "~/data/data";
 import { getVans } from "~/hooks/useVans";
 
 export default async function vansLoader(queryClient: QueryClient) {
-  // throw new Error("Failed to fetch data");
-  queryClient.prefetchQuery({
+  // const dataPromise = queryClient.ensureQueryData({
+  //   queryKey: ["vans"],
+  //   queryFn: async () => {
+  //     const vans = await getVans();
+  //     return vans;
+  //   },
+  //   staleTime: Infinity,
+  // });
+
+  const dataPromise = new Promise((resolve) => {
+    setTimeout(() => resolve(data), 5000);
+  });
+  return defer({ vans: dataPromise });
+}
+
+// These loader implementation prefetches the vans data and uses react-query
+// for actual data access within the component
+
+/*
+function loader(){
+    queryClient.prefetchQuery({
     queryKey: ["vans"],
     queryFn: async () => await getVans(),
   });
-  return "Prefetched vans";
 }
+*/
